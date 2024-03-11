0aa2c24c7d14c8f1df9c7770bdf7b67781d91706
8ogdanovv <8ogdanow@gmail.com>
2024-03-11 08:21:07 +0200

    vite config, add logo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import logo from '@/assets/logo.gif'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+ <h1>МедиаОфис</h1>
+ <a href="http://mediaoffice.com.ua/">
+  <img :src="logo" border="0" alt="МедіаОфис" width="201" height="26">
+ </a>
+ <div class="logo" width="201" height="26">
+
+ </div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  width: 201px;
+  height: 26px;
+  border: 1px solid var(--color-red);
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const isProduction = mode === 'production' || command === 'build'
+  const base = isProduction ? process.env.VITE_BASE_URL : '/'
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    base,
+  }
 })
