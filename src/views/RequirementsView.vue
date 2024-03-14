0bf3d04c19bd29c6f0fb0a0cd3b6efa9998ddc46
8ogdanovv <8ogdanow@gmail.com>
2024-03-14 06:34:32 +0200

    title based on page route

--- src/views/RequirementsView.vue
+++ src/views/RequirementsView.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 
+onMounted(() => document.title = 'Вимоги до макетів | МедіаОфіс')
 </script>
 
 <style lang="scss" scoped>
--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 
+onMounted(() => document.title = 'УФ-друк, лазерне різання, гравірування, згинання пластику, екосольвентний і сольвентний друк | МедіаОфіс')
 </script>
 
 <style lang="scss" scoped>
