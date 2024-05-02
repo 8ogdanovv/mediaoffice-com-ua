c37d9cabb3dafc77cb59d37294731388e594e284
8ogdanovv <8ogdanow@gmail.com>
2024-05-02 05:05:26 +0300

    Updates

---
+++ assets/RequirementsView-DBZIm7UG.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,b as s,w as a,r as c,o as n,e as r}from"./index-ahcRzn1t.js";const _={},i={class:"Yaroslav-the-Wise"};function d(l,f){const e=c("router-link");return n(),o("section",i,[s(e,{to:"/contacts"},{default:a(()=>[r("  ₴  ")]),_:1})])}const p=t(_,[["render",d],["__scopeId","data-v-a7aedb7e"]]);export{p as default};
